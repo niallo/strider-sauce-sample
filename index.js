1d70720ec7ee5a7ff290ebe5d36a66651975245d
Niall O'Higgins <niallo@unworkable.org>
2012-09-21 11:35:09 -0700

    add a trivial HTTP server

---
+++ index.js
@@ -0,0 +1,14 @@
+var app = require('tako')()
+
+// Return 200 on / and a little HTML doc too
+app.route('/')
+  .html(function (req, resp) {
+    console.log("server got request for /")
+    resp.end('<html><head>cool</head><body>this is a body</body></html>')
+  })
+  .methods('GET')
+
+var port = process.env.PORT || 8000
+app.httpServer.listen(port)
+
+console.log("tako server listening on port %d", port)
