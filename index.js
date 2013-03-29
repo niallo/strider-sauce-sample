eb4fe2f7c75a633a1e0b1e237467a45b1c7320c3
Niall O'Higgins <niallo@unworkable.org>
2013-03-29 16:51:21 -0700

    port to hapijs so it works on node 0.10

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
-var app = require('tako')()
+var Hapi = require('hapi');
 
-// Return 200 on / and a little HTML doc too
-app.route('/')
-  .html(function (req, resp) {
-    console.log("server got request for /")
-    resp.end('<html><head><title>Page Title</title></head><body>this is a body</body></html>')
-  })
-  .methods('GET')
-
+// Create a server with a host, port, and options
 var port = process.env.PORT || 8000
-app.httpServer.listen(port)
+var server = new Hapi.Server('localhost', port);
 
-console.log("tako server listening on port %d", port)
+// Define the route
+var index = {
+    handler: function (request) {
+        request.reply('<html><head><title>Page Title</title></head><body>this is a body</body></html>')
+    }
+};
+
+// Add the route
+server.addRoute({
+    method : 'GET',
+    path : '/',
+    config : index
+});
+
+// Start the server
+server.start();
+console.log("hapijs server listening on port %d", port)
