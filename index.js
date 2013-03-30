a2f1d10e6eb270e5e9e5f69070ed6e98972574c5
Niall O'Higgins <niallo@unworkable.org>
2013-03-29 17:00:48 -0700

    use base 10

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var Hapi = require('hapi');
 
 // Create a server with a host, port, and options
 var port = process.env.PORT || 8000
-var server = new Hapi.Server('localhost', parseInt(port));
+var server = new Hapi.Server('localhost', parseInt(port, 10));
 
 // Define the route
 var index = {
